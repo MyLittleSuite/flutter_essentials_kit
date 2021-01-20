875a479f814f6d5104f23ce9bb94e79652c627e8
Andrea Del Fante <andreadelfante94@gmail.com>
2021-01-20 21:50:51 +0100

    Add localized.dart, localized_exceptions.dart and localized_error.dart

---
+++ lib/errors/localized_error.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_essentials_kit/misc/localized.dart';
+
+/// Genetic Error class able to localize and translate messages to the users.
+abstract class LocalizedError extends Error implements Localized {}
