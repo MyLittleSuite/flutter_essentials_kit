842c5b30949af33cf22acec6cd7f6b8791486290
Andrea Del Fante <andreadelfante94@gmail.com>
2021-01-20 21:08:06 +0100

    Add Spacing widget

--- lib/flutter_essentials_kit.dart
+++ lib/flutter_essentials_kit.dart
@@ -2,6 +2,7 @@ library flutter_essentials_kit;
 
 // Widgets
 export 'widgets/courtesy_widget.dart';
+export 'widgets/spacing.dart';
 
 // Extensions
 export 'extensions/iterable.dart';
---
+++ lib/widgets/spacing.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// Create a space between widgets.
+class Spacing extends StatelessWidget {
+  final double horizontal;
+  final double vertical;
+
+  const Spacing({
+    Key key,
+    this.horizontal = 0,
+    this.vertical = 0,
+  }) : super(key: key);
+
+  const Spacing.horizontal(double value, {Key key})
+      : this(key: key, horizontal: value);
+
+  const Spacing.vertical(double value, {Key key})
+      : this(key: key, vertical: value);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        width: horizontal,
+        height: vertical,
+      );
+}
