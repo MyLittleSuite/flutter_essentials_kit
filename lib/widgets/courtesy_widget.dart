9b88d0f806c7e007b359bb32a110819af3dfdd50
Andrea Del Fante <andreadelfante94@gmail.com>
2021-03-18 20:00:44 +0100

    Add support of leading widget in courtesy widget, with padding and margin customizations

--- lib/widgets/courtesy_widget.dart
+++ lib/widgets/courtesy_widget.dart
@@ -1,28 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_essentials_kit/extensions/commons.dart';
 
-/// Create a courtesy widget with a required title, optional message, optional action and onPressed callback on that action.
+/// Create a courtesy widget with an optional leading widget, a required title, optional message, optional action and onPressed callback on that action.
 class CourtesyWidget extends StatelessWidget {
+  final Widget? leading;
   final String title;
   final String? message;
   final String? action;
   final VoidCallback? onPressed;
+  final EdgeInsets padding;
+  final EdgeInsets marginBetweenWidgets;
 
   CourtesyWidget({
     Key? key,
+    this.leading,
     required this.title,
     this.message,
     this.action,
     this.onPressed,
+    this.padding = const EdgeInsets.all(16),
+    this.marginBetweenWidgets = const EdgeInsets.only(top: 8),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => Padding(
-        padding: EdgeInsets.all(16),
+        padding: padding,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            _leading(context),
             _title(context),
             _message(context),
             _action(context),
@@ -30,7 +37,12 @@ class CourtesyWidget extends StatelessWidget {
         ),
       );
 
-  Widget _title(BuildContext context) => Center(
+  Widget _leading(BuildContext context) => Container(
+        child: leading?.let((it) => it),
+      );
+
+  Widget _title(BuildContext context) => Container(
+        margin: leading?.let((_) => marginBetweenWidgets) ?? EdgeInsets.zero,
         child: Text(
           title,
           style: Theme.of(context).textTheme.headline4,
@@ -39,17 +51,23 @@ class CourtesyWidget extends StatelessWidget {
       );
 
   Widget _message(BuildContext context) => Container(
-        margin: message != null ? EdgeInsets.only(top: 8) : EdgeInsets.zero,
-        child: message != null ? Text(message!) : null,
+        margin: message?.let((_) => marginBetweenWidgets) ?? EdgeInsets.zero,
+        child: message?.let(
+          (it) => Text(
+            it,
+            style: Theme.of(context).textTheme.bodyText2,
+            textAlign: TextAlign.center,
+          ),
+        ),
       );
 
-  Widget _action(BuildContext context) => action != null
-      ? Padding(
-          padding: EdgeInsets.only(top: 8),
-          child: ElevatedButton(
+  Widget _action(BuildContext context) => Container(
+        margin: action?.let((_) => marginBetweenWidgets) ?? EdgeInsets.zero,
+        child: action?.let(
+          (it) => ElevatedButton(
             child: Text(action!),
             onPressed: onPressed,
           ),
-        )
-      : Container();
+        ),
+      );
 }
